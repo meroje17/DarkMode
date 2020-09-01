a17d972424c825a758b391a8995b63037be73504
Jérôme Guèrin <guerinjerome@outlook.fr>
2020-09-01 16:09:32 +0200

    build(xcode): project structure implemented

---
+++ DarkMode/DarkMode/ColorsTool/Color.swift
@@ -0,0 +1,9 @@
+//
+//  Color.swift
+//  DarkMode
+//
+//  Created by Jérôme Guèrin on 01/09/2020.
+//  Copyright © 2020 Jérôme Guèrin. All rights reserved.
+//
+
+import Foundation
---
+++ DarkMode/DarkMode/Controller/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  DarkMode
+//
+//  Created by Jérôme Guèrin on 01/09/2020.
+//  Copyright © 2020 Jérôme Guèrin. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
