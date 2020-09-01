e83551e42d73a9b032bfaf41402badc5d8d85a97
Jérôme Guèrin <guerinjerome@outlook.fr>
2020-09-01 17:50:13 +0200

    build(*): application finished

--- DarkMode/DarkMode/ColorsTool/Color.swift
+++ DarkMode/DarkMode/ColorsTool/Color.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 enum ColorMode {
-    case White, Black
+    case white, black
 }
 
 final class Color {
@@ -18,19 +18,22 @@ final class Color {
     var forTheView: UIColor!
     var forTheSubview: UIColor!
     var forTheBackground: UIColor!
+    var forTheBackgroundButton: UIColor!
     
     func changeUI(with colorMode: ColorMode) {
         switch colorMode {
-        case .White:
+        case .white:
             forTheText = UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
             forTheView = UIColor.white
             forTheSubview = UIColor(red: 250/255, green: 250/255, blue: 250/255, alpha: 1)
             forTheBackground = UIColor(red: 235/255, green: 235/255, blue: 235/255, alpha: 1)
-        case .Black:
+            forTheBackgroundButton = UIColor(red: 250/255, green: 250/255, blue: 250/255, alpha: 1)
+        case .black:
             forTheText = UIColor.white
-            forTheView = UIColor(red: 33/255, green: 33/255, blue: 33/255, alpha: 1)
-            forTheSubview = UIColor(red: 250/255, green: 250/255, blue: 250/255, alpha: 1)
-            forTheBackground = UIColor(red: 192/255, green: 192/255, blue: 192/255, alpha: 1)
+            forTheView = UIColor(red: 60/255, green: 60/255, blue: 60/255, alpha: 1)
+            forTheSubview = UIColor(red: 130/255, green: 130/255, blue: 130/255, alpha: 1)
+            forTheBackground = UIColor(red: 128/255, green: 128/255, blue: 128/255, alpha: 1)
+            forTheBackgroundButton = UIColor(red: 180/255, green: 180/255, blue: 180/255, alpha: 1)
         }
     }
 }
--- DarkMode/DarkMode/Controller/MainViewController.swift
+++ DarkMode/DarkMode/Controller/MainViewController.swift
@@ -10,19 +10,56 @@ import UIKit
 
 final class MainViewController: UIViewController {
 
+    // MARK: - Property
+    
+    private var colorsReference = Color()
+    
     // MARK: - Outlets
     
+    // Outlets to rounded
     @IBOutlet private var viewToRoundedInFive: [UIView]!
     @IBOutlet private var viewWithBorder: [UIView]!
     @IBOutlet private weak var customTabBar: UIView!
     
+    // Outlets to colored
+    @IBOutlet private var textsCollection: [UILabel]!
+    @IBOutlet private var viewsColor: [UIView]!
+    @IBOutlet private var subviewsColor: [UIView]!
+    @IBOutlet private var backgroundViewColor: UIView!
+    
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    
+    // MARK: - Initializer
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        colorsReference.changeUI(with: .white)
         initUI()
     }
     
+    // MARK: - Action
+    
+    @IBAction private func valueChanged() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            colorsReference.changeUI(with: .white)
+        case 1:
+            colorsReference.changeUI(with: .black)
+        default:
+            colorsReference.changeUI(with: .white)
+        }
+        initUI()
+    }
+    
+    // MARK: - Private functions
+    
     private func initUI() {
+        roundedView()
+        assignColors()
+    }
+    
+    private func roundedView() {
         for view in viewToRoundedInFive {
             view.layer.cornerRadius = 8
         }
@@ -32,5 +69,19 @@ final class MainViewController: UIViewController {
             view.layer.borderColor = UIColor(displayP3Red: 235/255, green: 235/255, blue: 235/255, alpha: 1).cgColor
         }
         customTabBar.layer.cornerRadius = 20
+    }
+    
+    private func assignColors() {
+        for text in textsCollection {
+            text.textColor = colorsReference.forTheText
+        }
+        for view in viewsColor {
+            view.backgroundColor = colorsReference.forTheView
+        }
+        for subview in subviewsColor {
+            subview.backgroundColor = colorsReference.forTheSubview
+        }
+        backgroundViewColor.backgroundColor = colorsReference.forTheBackground
+        segmentedControl.backgroundColor = colorsReference.forTheBackgroundButton
     }
 }
